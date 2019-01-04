62797a01c2a239435b7996b7bbc7f3566e808753
AlphaKretin <deewhydeeecks@gmail.com>
2019-01-04 15:05:37 +1030

    Update premake for OS X build

--- premake4.lua
+++ premake4.lua
@@ -1,7 +1,6 @@
 local project_name = "ygopen-srv"
 local ygopen_dir   = "../ygopen"
-local sqlite_dir = "../sqlite3"
-local json_dir = "../json-develop/include"
+local json_dir     = "../json-develop/include"
 
 solution(project_name)
 	location(".")
@@ -9,41 +8,30 @@ solution(project_name)
 	objdir("obj")
 	
 	configurations({"Debug", "Release"})
-	
-	configuration("windows")
-        defines { "WIN32", "_WIN32", "NOMINMAX" }
 
 	configuration("Debug")
-        symbols("On")
+		flags("Symbols")
 		defines("_DEBUG")
 		targetdir("bin/debug")
 
 	configuration("Release")
-        optimize("Speed")
+		flags("OptimizeSpeed")
 		defines("_RELEASE")
 		targetdir("bin/release")
 
 	include(ygopen_dir)
-	configuration "windows"
-		include(sqlite_dir)
-		
-	configuration "vs*"
-        characterset("MBCS")
 
 	project(project_name)
 		kind("ConsoleApp")
 		flags("ExtraWarnings")
 		defines("ASIO_STANDALONE")
 		files({"*.hpp", "*.cpp"})
-		includedirs(ygopen_dir)
-		configuration "windows"
-			includedirs (sqlite_dir)
-			includedirs (json_dir)
+		includedirs {ygopen_dir, json_dir}
 		links({"ygopen", "sqlite3"})
 
+		configuration("osx")
+			buildoptions("-pedantic  --std=c++11")
+			links({"dl", "pthread"})
 		configuration("not windows")
 			buildoptions("-pedantic")
 			links({"dl", "pthread"})
-			
-		configuration "vs*"
-			characterset("MBCS")
